9576de3eeb21c4d273d9ffcd8922d64303478380
Lauren Giordano <laurengiordano94@gmail.com>
2023-05-10 13:55:43 +1000

    done with lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "brown", "placid", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "M", "black", "placid", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 1, "F", "brown", "violent", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 10, "F", "spotted", "placid", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 8, "F", "black", "unpredictable", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 5, "M", "brown", "violent", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 6, "M", "brown", "unpredictable", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, "F", "spotted", "placid", 1);
